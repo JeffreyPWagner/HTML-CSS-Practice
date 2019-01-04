2cf535bc1cf7fcee2bfc475496fa4be395496588
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2019-01-04 14:08:40 -0500

    Added nav function and smooth scroll

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
     
+    /* Sticky navigation */
+    $('.js-section-features').waypoint(function(direction) {
+        if (direction == "down"){
+            $('nav').addClass('sticky');
+        }
+        else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
     
+    /* Scroll buttons */
+    $('.js-scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js-section-plans').offset().top}, 1000);
+    });
+    
+    $('.js-scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000);
+    });
     
 });
