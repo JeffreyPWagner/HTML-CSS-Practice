a319dd5578f9f93a9ff3c69dec1d84e6f674aae3
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2019-01-05 11:11:54 -0500

    added animations on scroll

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -21,4 +21,29 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $('.js-section-features').offset().top}, 1000);
     });
     
+    /* Animations on scroll */
+    
+    $('.js-wp-1').waypoint(function(direction){
+        $('.js-wp-1').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js-wp-2').waypoint(function(direction){
+        $('.js-wp-2').addClass('animated fadeInUp');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js-wp-3').waypoint(function(direction){
+        $('.js-wp-3').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js-wp-4').waypoint(function(direction){
+        $('.js-wp-4').addClass('animated pulse');
+    },{
+        offset: '50%'
+    });
 });
